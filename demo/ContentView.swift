eebaaff3293a58ad5f04af93b9ae857217fcb586
Administrator <labrada.alexei@gmail.com>
2023-12-03 11:29:07 +0300

    Create project

---
+++ demo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  demo
+//
+//  Created by Administrator on 03.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by Administrator on 03.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
